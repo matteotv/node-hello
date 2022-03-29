d86f572e28d53e759cab018911ad79cd3b1aed6b
matteotv <matteotv.it@gmail.com>
2022-03-29 23:33:57 +0100

    feature/1: add new lamda name into terraform configuration file

--- terraform/main.tf
+++ terraform/main.tf
@@ -25,7 +25,7 @@ resource "aws_iam_role" "iam_for_lambda" {
 EOF
 }
 resource "aws_lambda_function" "test_lambda" {
-  filename      = "func.zip"
+  filename      = "lambdatest.zip"
   function_name = "lambda_node_test_project"
   role          = aws_iam_role.iam_for_lambda.arn
   handler       = "index.test"
@@ -33,7 +33,7 @@ resource "aws_lambda_function" "test_lambda" {
   # The filebase64sha256() function is available in Terraform 0.11.12 and later
   # For Terraform 0.11.11 and earlier, use the base64sha256() function and the file() function:
   # source_code_hash = "${base64sha256(file("lambda_function_payload.zip"))}"
-  source_code_hash = filebase64sha256("func.zip")
+  source_code_hash = filebase64sha256("lambdatest.zip")
 
   runtime = "nodejs12.x"
 
